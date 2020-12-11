67fb97386116a994fa708b4b733fe33951521321
chenkail <40770208+Chenkail@users.noreply.github.com>
2020-12-11 02:41:53 -0800

    Update conf.py
    Co-Authored-By: yrkv <11140316+yrkv@users.noreply.github.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 
 import os
 import sys
-sys.path.insert(os.path.abspath('../../dokusu/'))
+sys.path.insert(os.path.abspath(0, '../..'))
 
 
 # -- Project information -----------------------------------------------------
